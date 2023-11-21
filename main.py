58f1ebaf44defe7b6e1bbe7f2d57051d1f11baf2
JungWanSEO <sjwan0326@gmail.com>
2023-11-21 15:35:14 +0900

    테이블 로직 추가
    DB 테이블 생성 로직 추가

--- main.py
+++ main.py
@@ -11,6 +11,17 @@ conn = sqlite3.connect('database.db', check_same_thread=False)
 #
 cur = conn.cursor()
 
+cur.execute(f"""
+            CREATE TABLE IF NOT EXISTS items(
+	            id INTEGER PRIMARY KEY,
+	            title TEXT NOT NULL,
+	            image BLOB,
+	            price INTEGER NOT NULL,
+	            description TEXT,
+	            place TEXT NOT NULL,
+	            insertAt INTEGER NOT NULL
+            );
+            """)
 
 class Chat(BaseModel):
     id: int
